38a7d55c0be4f228797693a95681664ae728d688
rimv175 <rimv715@gmail.com>
2026-02-13 20:31:29 -0600

    fix flake

--- flake.nix
+++ flake.nix
@@ -25,6 +25,7 @@
           home-manager.useGlobalPkgs = true;
           home-manager.useUserPackages = true;
           home-manager.users.rimv = import ./home.nix;
+          home-manager.extraSpecialArgs = { inherit inputs; };
         }
       ];
     };
