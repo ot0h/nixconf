079b4ac0191a8d1987191693ca229aac7d1d1bde
rimv175 <rimv715@gmail.com>
2026-02-16 17:12:23 -0600

    sane

--- configuration.nix
+++ configuration.nix
@@ -153,13 +153,13 @@
       epkowa
       utsushi
 
-      (writeTextFile {
-        name = "epson2.conf";
-        text = "
-          net  192.168.0.129
-          ";
-        destination = "/etc/sane.d/epson2.conf";
-      })
+      # (writeTextFile {
+      #   name = "epson2.conf";
+      #   text = ''
+      #     net  192.168.0.129
+      #     '';
+      #   destination = "/etc/sane.d/epson2.conf";
+      # })
     ];
   };
   services.pulseaudio.enable = false;
