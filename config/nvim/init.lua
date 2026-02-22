0edd1cf5b1ae3ab3384934a64523e1862d028105
rrimv <rimv715@gmail.com>
2026-02-22 17:13:26 -0600

    Agregando Oil

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -78,12 +78,13 @@ vim.pack.add({
 	},
 
 	{
-		src = "https://github.com/neovim/nvim-lspconfig",
-		version = "master",
+		src = "https://github.com/folke/ts-comments.nvim",
+		version = "main",
 	},
 
 	{
-		src = "https://github.com/folke/ts-comments.nvim",
+		-- Oil
+		src = "https://github.com/stevearc/oil.nvim",
 		version = "main",
 	},
 
--- config/nvim/lua/config/keymaps.lua
+++ config/nvim/lua/config/keymaps.lua
@@ -1,7 +1,8 @@
 local opts = { noremap = true, silent = true }
 local keymap = vim.keymap.set
 
-keymap("n", "-", ":lua MiniFiles.open()<CR>", opts)
+keymap("n", "-", ":Oil<CR>", opts)
+keymap("n", "_", ":lua MiniFiles.open()<CR>", opts)
 keymap("n", "<leader>", ":WhichKey<CR>", opts)
 
 -- SmartSplits
