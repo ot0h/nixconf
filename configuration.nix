1bfea57614831591feaf7ffbc51f219aa4a46a35
rrimv <rimv715@gmail.com>
2026-03-03 11:58:14 -0600

    quitando el displaymanager para que solo sea con la consola

--- configuration.nix
+++ configuration.nix
@@ -59,7 +59,7 @@
     openFirewall = true;
   };
 
-  services.displayManager.gdm.enable = true;
+  services.displayManager.gdm.enable = false;
   services.desktopManager.gnome.enable = false;
 
   services.gnome = {
