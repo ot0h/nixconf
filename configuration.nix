b9226ef898b355edde68b7a9f56b3080db684f53
rrimv <rimv715@gmail.com>
2026-03-04 16:31:15 -0600

    Quitando el inputs del homenanger del gazelle

--- configuration.nix
+++ configuration.nix
@@ -59,7 +59,7 @@
     openFirewall = true;
   };
 
-  services.displayManager.gdm.enable = false;
+  services.displayManager.gdm.enable = true;
   services.desktopManager.gnome.enable = false;
 
   services.gnome = {
