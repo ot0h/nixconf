023b51fdaf7b9131bd49cd961f0b5d32f4e69813
rrimv <rimv715@gmail.com>
2026-03-09 09:41:27 -0600

    arreglando fixes

--- modules/yazi.nix
+++ modules/yazi.nix
@@ -1,13 +1,25 @@
 {pkgs, ...}: {
   programs.yazi = {
     enable = true;
-
-    plugins = with pkgs; {
-      gvfs = yaziPlugins.gvfs;
+    plugins = with pkgs.yaziPlugins; {
+      gvfs = gvfs;
     };
   };
-  xdg.configFile."yazi" = {
-    source = ../config/yazi;
-    recursive = true;
+
+  xdg.configFile = {
+    "yazi/yazi.toml".source = ../config/yazi/yazi.toml;
+    "yazi/keymap.toml".source = ../config/yazi/keymap.toml;
+    "yazi/theme.toml".source = ../config/yazi/theme.toml;
+    "yazi/init.lua".source = ../config/yazi/init.lua;
+
+    "yazi/scripts" = {
+      source = ../config/yazi/scripts;
+      recursive = true;
+    };
+
+    "yazi/plugins" = {
+      source = ../config/yazi/plugins;
+      recursive = true;
+    };
   };
 }
