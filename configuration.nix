6ce50f0c81cb631db68980b5277e4e809978567e
rrimv <rimv715@gmail.com>
2026-03-14 19:49:28 -0600

    cosas

--- configuration.nix
+++ configuration.nix
@@ -59,8 +59,7 @@
     openFirewall = true;
   };
 
-  services.getty.autologinUser = "rimv";
-  services.displayManager.gdm.enable = false;
+  services.displayManager.gdm.enable = true;
   services.desktopManager.gnome.enable = false;
 
   services.gnome = {
