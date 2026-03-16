75286ae430c493f2f457fc4d3244287593c01205
rrimv <rimv715@gmail.com>
2026-03-16 09:32:31 -0600

    cord nvim

---
+++ config/nvim/lua/plugins/misc.lua
@@ -0,0 +1,10 @@
+require("cord").setup({
+	editor = {
+		client = "neovim",
+		tooltip = "El unico Text Editor que me gusta xd",
+	},
+	display = {
+		theme = "minecraft",
+		flavor = "accent",
+	},
+})
