9f3b00712f4a72ea30958709a771faef71360740
rrimv <rimv715@gmail.com>
2026-03-17 08:25:44 -0600

    Cosas

--- modules/ghostty.nix
+++ modules/ghostty.nix
@@ -2,8 +2,14 @@
   programs.ghostty = {
     enable = true;
 
+    package = pkgs.ghostty;
+
     settings = {
-      command = "env GTK_IM_MODULE=simple";
+      command = "env GTK_IM_MODULE=simple tmux -CC attach -t default || env GTK_IM_MODULE=simple tmux -CC new -s default";
+      shell-integration = "fish";
+      font-family = "IosevkaTermSlab Nerd Font";
+      font-size = 12;
+      confirm-close-surface = false;
     };
   };
 }
