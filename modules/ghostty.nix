78bd69bde62cab69a2fda5d79fbc3fae38e50b93
rrimv <rimv715@gmail.com>
2026-03-17 08:28:15 -0600

    Config para ghostty

--- modules/ghostty.nix
+++ modules/ghostty.nix
@@ -1,10 +1,16 @@
-{pkgs, ...}: {
+{
+  pkgs,
+  config,
+  ...
+}: {
   programs.ghostty = {
     enable = true;
 
     package = pkgs.ghostty;
 
     settings = {
+      config-file = "${config.home.homeDirectory}/.cache/wal/ghostty.conf";
+
       command = "env GTK_IM_MODULE=simple tmux -CC attach -t default || env GTK_IM_MODULE=simple tmux -CC new -s default";
       shell-integration = "fish";
       font-family = "IosevkaTermSlab Nerd Font";
