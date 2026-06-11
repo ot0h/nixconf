f5d13b3029a2f9f84da60be43b6c52e09b76dd2f
numbpi <rimv715@gmail.com>
2026-06-11 12:51:35 -0600

    update obsidian nvim

--- config/nvim/lua/plugins/markdown.lua
+++ config/nvim/lua/plugins/markdown.lua
@@ -1,12 +1,26 @@
-require("obsidian").setup({
-	legacy_commands = false,
-	workspaces = {
-		{
-			name = "personal",
-			path = "~/Documents/Notas",
+local ok, obs_or_err = pcall(require, "obsidian")
+if not ok then
+	vim.notify("obsidian.nvim not available: " .. tostring(obs_or_err), vim.log.levels.WARN)
+else
+	local setup_ok, setup_err = pcall(obs_or_err.setup, {
+		legacy_commands = false,
+		workspaces = {
+			{
+				name = "personal",
+				path = "~/Documents/Notas",
+			},
 		},
-	},
-})
+	})
+	if not setup_ok then
+		vim.schedule(function()
+			vim.notify(
+				"obsidian.nvim skipped: " .. tostring(setup_err),
+				vim.log.levels.WARN,
+				{ title = "obsidian.nvim" }
+			)
+		end)
+	end
+end
 
 require("markview").setup({
 	---@type markview.config
