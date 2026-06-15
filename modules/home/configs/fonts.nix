033e4610736b60f37d777417bb114030a050fc4a
numbpi <rimv715@gmail.com>
2026-06-15 11:54:48 -0600

    Ajustando Fonts

--- modules/home/configs/fonts.nix
+++ modules/home/configs/fonts.nix
@@ -3,19 +3,15 @@
     jetbrains-mono
     caskaydia-cove
     iosevka-term-slab
-    bigblue-terminal
   ];
 in {
   home.packages = with pkgs;
     [
       pixel-code
       iosevka
-      monocraft
-      victor-mono
       liberation_ttf
       noto-fonts
       noto-fonts-cjk-sans
-      monaspace
     ]
     ++ nerdFonts;
 }
