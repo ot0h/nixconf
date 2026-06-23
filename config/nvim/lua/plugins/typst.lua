21464926e9ec577ebb437bdcdf8e3de04aebce5b
numbpi <rimv715@gmail.com>
2026-06-23 15:44:45 -0600

    fix TypstPreview

--- config/nvim/lua/plugins/typst.lua
+++ config/nvim/lua/plugins/typst.lua
@@ -38,7 +38,7 @@ require("typst-preview").setup({
 
 	-- A list of extra arguments (or nil) to be passed to previewer.
 	-- For example, extra_args = { "--input=ver=draft", "--ignore-system-fonts" }
-	extra_args = nil,
+	extra_args = { "--verbose" },
 
 	-- This function will be called to determine the root of the typst project
 	get_root = function(path_of_main_file)
