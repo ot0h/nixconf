3d6d88d36517a87073ea54c84d158fc2d8402e8e
numbpi <rimv715@gmail.com>
2026-06-25 16:46:40 -0600

    quitando esto

--- config/nvim/lua/config/autocmds.lua
+++ config/nvim/lua/config/autocmds.lua
@@ -10,28 +10,6 @@ vim.api.nvim_create_autocmd("LspAttach", {
 	end,
 })
 
--- Comando para actualizar Vim Pack
-vim.api.nvim_create_user_command("PackUpdate", function()
-	vim.pack.update()
-end, { desc = "Actualizar Plugins" })
-
--- Actualizar el Quickfix
-vim.api.nvim_create_autocmd("DiagnosticChanged", {
-	callback = function()
-		-- Solo actualiza si la Quickfix ya está abierta
-		local qf_exists = false
-		for _, win in pairs(vim.fn.getwininfo()) do
-			if win.quickfix == 1 then
-				qf_exists = true
-			end
-		end
-
-		if qf_exists then
-			vim.diagnostic.setqflist({ open = false })
-		end
-	end,
-})
-
 -- Teners Folds Persistentes
 vim.api.nvim_create_autocmd({ "BufWinLeave" }, {
 	pattern = "*",
