b4db42916c4cd2fc0e819b1ca381d1f25ec9d72b
numbpi <rimv715@gmail.com>
2026-06-28 18:39:59 -0600

    Aajustes

--- hosts/laptop-dell/configuration.nix
+++ hosts/laptop-dell/configuration.nix
@@ -189,7 +189,7 @@ in {
   programs.git = {
     enable = true;
     config = {
-      user.name = "numbpi";
+      user.name = "typsit";
       user.email = "rimv715@gmail.com";
       init.defaultBranch = "main";
       pull.rebase = true;
