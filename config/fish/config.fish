4398faadcf4b9dc7a9b58f1756163b3dd8da5ec4
qisvo <rimv715@gmail.com>
2026-07-02 15:35:07 -0600

    agregando el path de go

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -2,6 +2,7 @@ set -g fish_greeting ""
 complete -f -c dotnet -a "(dotnet complete (commandline -cp))"
 set -x TMUX_CONF ~/.config/tmux/tmux.conf
 set -g fish_key_bindings fish_vi_key_bindings
+set -gx PATH $PATH $HOME/go/bin
 
 if not test -f /tmp/kitty-opacity.conf
     echo "background_opacity 0.8" >/tmp/kitty-opacity.conf
