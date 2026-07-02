79f9166106735b2702a56e3bd68c77c6680c5da0
qisvo <rimv715@gmail.com>
2026-07-02 14:13:12 -0600

    quitando helium

--- modules/home/entretenimiento/browser.nix
+++ modules/home/entretenimiento/browser.nix
@@ -6,7 +6,6 @@
   home.packages = with pkgs; [
     inputs.zen-browser.packages.${stdenv.hostPlatform.system}.default
 
-    # firefox
-    (pkgs.callPackage ./helium.nix {})
+    # (pkgs.callPackage ./helium.nix {})
   ];
 }
