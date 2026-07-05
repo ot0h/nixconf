b932e482eb6fc115f99475acf7883bdb91d8b8aa
qisvo <rimv715@gmail.com>
2026-07-04 19:45:21 -0600

    rename de snacks

--- config/nvim/lua/config/autocmds.lua
+++ config/nvim/lua/config/autocmds.lua
@@ -1,3 +1,5 @@
+local Snacks = require("snacks")
+
 -- Activar Hints en los servidores declarados
 vim.api.nvim_create_autocmd("LspAttach", {
 	callback = function(ev)
@@ -22,5 +24,21 @@ vim.api.nvim_create_autocmd({ "BufWinEnter" }, {
 	pattern = "*",
 	callback = function()
 		vim.cmd("silent! loadview")
+	end,
+})
+
+vim.api.nvim_create_autocmd("User", {
+	pattern = "OilActionsPost",
+	callback = function(event)
+		if event.data.actions[1].type == "move" then
+			Snacks.rename.on_rename_file(event.data.actions[1].src_url, event.data.actions[1].dest_url)
+		end
+	end,
+})
+
+vim.api.nvim_create_autocmd("User", {
+	pattern = "MiniFilesActionRename",
+	callback = function(event)
+		Snacks.rename.on_rename_file(event.data.from, event.data.to)
 	end,
 })
