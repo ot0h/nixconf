dd359218ee977e39ecb38e52c894031f02bafb42
qisvo <rimv715@gmail.com>
2026-07-08 15:48:03 -0600

    ajustes

--- config/hypr/configs/autostart.lua
+++ config/hypr/configs/autostart.lua
@@ -3,7 +3,7 @@
 -------------------
 
 hl.on("hyprland.start", function()
-	hl.exec_cmd("awww-daemon & clipse -listen & hypridle")
+	hl.exec_cmd("hyprpaper & clipse -listen & hypridle")
 
 	hl.exec_cmd([[
 		sh -c 'watchexec -r \
