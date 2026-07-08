52707dd57f717821d608ae44439337e0320b5182
qisvo <rimv715@gmail.com>
2026-07-08 15:40:05 -0600

    ajustes

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -32,6 +32,7 @@ vim.pack.add({
 	{ src = "https://github.com/ThePrimeagen/refactoring.nvim", version = "master" },
 	{ src = "https://github.com/lewis6991/async.nvim", version = "main" },
 	{ src = "https://github.com/nvim-mini/mini.splitjoin", version = "stable" },
+	{ src = "https://github.com/nvim-mini/mini.base16", version = "stable" },
 
 	-- Dependencias
 	{ src = "https://github.com/MunifTanjim/nui.nvim" },
