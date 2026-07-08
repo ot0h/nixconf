7fad15472982eb7c1bfcf015c24457a44fc474c7
qisvo <rimv715@gmail.com>
2026-07-08 17:15:04 -0600

    ajustes

--- config/hypr/configs/autostart.lua
+++ config/hypr/configs/autostart.lua
@@ -8,7 +8,6 @@ hl.on("hyprland.start", function()
 	hl.exec_cmd([[
 		sh -c 'watchexec -r \
 		--watch ~/.config/ags \
-		--watch ~/.cache/wal/colors.css \
 		-- "ags quit; ags run & ags request reload; hyprctl reload"'
 	]])
 end)
