1679fa56c5184e7486ab10c0a9c2e657735c15e5
qisvo <rimv715@gmail.com>
2026-07-08 16:09:05 -0600

    migrando a stylix

--- config/nvim/colors.lua
+++
@@ -1,1 +0,0 @@
-return dofile(vim.fn.expand("~/.config/stylix-colors/colors.lua"))
--- config/nvim/lua/plugins/colors.lua
+++ config/nvim/lua/plugins/colors.lua
@@ -2,6 +2,6 @@ local palette = dofile(vim.fn.expand("~/.config/stylix-colors/palette.lua"))
 
 require("mini.base16").setup({
 	palette = palette,
-	use_cterm = true,
+	use_cterm = false,
 	plugins = { default = true },
 })
