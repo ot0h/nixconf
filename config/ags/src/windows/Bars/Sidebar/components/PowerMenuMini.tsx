d553b6a0f256e800ffbf5e5ac5a5ca793241dbcf
qisvo <rimv715@gmail.com>
2026-07-07 20:56:19 -0600

    agregando gestures al componente de notis en el sidebar

--- config/ags/src/windows/Bars/Sidebar/components/PowerMenuMini.tsx
+++ config/ags/src/windows/Bars/Sidebar/components/PowerMenuMini.tsx
@@ -32,11 +32,28 @@ export default function PowerMenuMini() {
     },
   ])
 
+  const setupClickGestures = (self: Gtk.Button) => {
+    const gesture = new Gtk.GestureClick({ button: 0 })
+
+    gesture.connect('released', (gesture) => {
+      const boton = gesture.get_current_button()
+
+      if (boton === Gdk.BUTTON_PRIMARY) {
+        toggle()
+      } else {
+        execAsync('ags request toggle AppLauncher')
+      }
+    })
+
+    self.add_controller(gesture)
+  }
+
   return (
     <box orientation={1} class="powermenumini">
       <button
         onClicked={toggle}
         cursor={Gdk.Cursor.new_from_name('pointer', null)}
+        $={setupClickGestures}
       >
         <label class="icon-iso" label="" hexpand halign={Gtk.Align.CENTER} />
       </button>
