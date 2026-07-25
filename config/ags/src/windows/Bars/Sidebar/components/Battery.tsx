868074031fdb90686274c2352a073bfb7bff2cd6
ot0h <rimv715@gmail.com>
2026-07-25 15:43:01 -0600

    falta ajustar bien lo de la bateria

--- config/ags/src/windows/Bars/Sidebar/components/Battery.tsx
+++ config/ags/src/windows/Bars/Sidebar/components/Battery.tsx
@@ -1,23 +1,34 @@
-import AstalBattery from 'gi://AstalBattery?version=0.1'
-import { createBinding } from 'gnim'
+import { createPoll } from 'ags/time'
+import { getBatteryInfo } from '@utils'
 import { Gtk } from 'ags/gtk4'
 
 export default function Battery() {
-  const battery = AstalBattery.get_default()
-  const percent = createBinding(
-    battery,
-    'percentage'
-  )((p) => `Bat: ${Math.floor(p * 100)}%`)
+  const battery = createPoll({ percent: 0, status: 'Unknown' }, 2000, () =>
+    getBatteryInfo()
+  )
+
+  const getIcon = (b: { percent: number; status: string }): string => {
+    if (b.status === 'Charging') return '󰂄'
+
+    const p = b.percent
+    if (p > 0 && p <= 20) return '󰁺'
+    if (p >= 20 && p <= 29) return '󰁻'
+    if (p >= 30 && p <= 39) return '󰁼'
+    if (p >= 40 && p <= 49) return '󰁽'
+    if (p >= 50 && p <= 59) return '󰁾'
+    if (p >= 60 && p <= 69) return '󰁿'
+    if (p >= 70 && p <= 79) return '󰂀'
+    if (p >= 80 && p <= 89) return '󰂁'
+    if (p >= 90 && p <= 95) return '󰂂'
+    return '󰁹'
+  }
 
   return (
-    <>
-      <image
-        class="bateria"
-        tooltip_text={percent}
-        iconName={createBinding(battery, 'iconName')}
-        valign={Gtk.Align.CENTER}
-        halign={Gtk.Align.CENTER}
+    <box class="bateria" spacing={5} halign={Gtk.Align.CENTER}>
+      <label
+        label={battery.as(getIcon)}
+        tooltip_text={battery.as((b) => `Bat: ${b.percent}%\n(${b.status})`)}
       />
-    </>
+    </box>
   )
 }
